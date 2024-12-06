c74388a4bde4f1a4b5a6fd077bd7b6d67e97d355
LilDavid <dat.dotuan@hcmut.edu.vn>
2024-12-06 21:31:41 +0700

    Đổi lại gọi hàm thay vì gọi thủ tục

--- project-mvc-xampp/controllers/adminController.js
+++ project-mvc-xampp/controllers/adminController.js
@@ -1,23 +1,31 @@
 const db = require('../models/db'); 
 
 const getNumberProductSoldbyId = (req, res) => {
-     const { MaNguoiBan } = req.body;  
+    const { MaNguoiBan } = req.body;
 
     if (!MaNguoiBan) {
         return res.status(400).json({ message: 'Mã người bán không được để trống' });
     }
-    db.execute('CALL TinhSoLuongSanPhamDaBan(?)', [MaNguoiBan], (err, results) => {
+
+    const query = 'SELECT TinhSoLuongSanPhamDaBan(?) AS SoLuongDaBan';
+
+    db.execute(query, [MaNguoiBan], (err, results) => {
         if (err) {
-            console.error(err);
+            console.error('Lỗi khi gọi hàm: ', err);
             return res.status(500).json({ message: 'Lỗi khi truy vấn cơ sở dữ liệu' });
         }
-        if (results[0][0]['Số Lượng Sản Phẩm Đã Bán'] !== null) {
-            return res.json({ SoLuongDaBan: results[0][0]['Số Lượng Sản Phẩm Đã Bán'] });
+
+        const SoLuongDaBan = results[0]?.SoLuongDaBan;
+
+        if (SoLuongDaBan !== null && SoLuongDaBan > 0) {
+            return res.json({ SoLuongDaBan });
         } else {
-            return res.json({ message: results[0][0]['Thông Báo'] });
+            return res.json({ message: 'Không có sản phẩm hoặc không tồn tại người bán.' });
         }
     });
 };
+
+
 
 const getRating = (req, res) => {
     const { MaCuaHang } = req.body;  
@@ -25,18 +33,24 @@ const getRating = (req, res) => {
     if (!MaCuaHang) {
         return res.status(400).json({ message: 'Mã cửa hàng không hợp lệ' });
     }
-    db.execute('CALL KiemTraDanhGiaCuaHang(?)', [MaCuaHang], (err, results) => {
+
+    const query = 'SELECT TinhDiemTrungBinhCuaHang(?) AS `Số Sao Trung Bình Của Cửa Hàng`';
+
+    db.execute(query, [MaCuaHang], (err, results) => {
         if (err) {
-        console.error('Lỗi khi gọi thủ tục: ', err);
-        return res.status(500).json({ message: 'Lỗi khi gọi thủ tục' });
+            console.error('Lỗi khi gọi hàm: ', err);
+            return res.status(500).json({ message: 'Lỗi khi gọi hàm' });
         }
-        if (results[0][0]['Số Sao Trung Bình Của Cửa Hàng']) {
-        res.json({ 'Số Sao Trung Bình Của Cửa Hàng': results[0][0]['Số Sao Trung Bình Của Cửa Hàng'] });
+
+        const rating = results[0]?.['Số Sao Trung Bình Của Cửa Hàng'];
+        if (rating !== null && rating !== undefined) {
+            res.json({ 'Số Sao Trung Bình Của Cửa Hàng': rating });
         } else {
-        res.json({ message: 'Cửa hàng này không có sản phẩm nào hoặc không tồn tại.' });
+            res.json({ message: 'Cửa hàng này không có sản phẩm nào được đánh giá hoặc không tồn tại.' });
         }
     });
 };
+
 
 module.exports = {
     getNumberProductSoldbyId,
